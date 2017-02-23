e24605bd5b668e57d4107e98dec8da0f1fc2c286
Maureen_Ding <md3474@tc.columbia.edu>
2017-02-23 16:30:08 -0500

    Create the Student Class

---
+++ app/src/main/java/Student.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Maureen_Ding on 2/23/17.
+ */
+
+public class Student {
+    String name;
+    String uni;
+    String gender;
+    boolean graduate;
+
+    public Student(String name,String uni,String gender, boolean graduate){
+        this.name = name;
+        this.uni = uni;
+        this.gender = gender;
+        this.graduate = graduate;
+    }
+}
